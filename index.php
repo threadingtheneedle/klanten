c4c67cecaf520d138db161811d3bacb31e47f5f8
threadingtheneedle <gino.xu@edu-kw1c.nl>
2026-03-02 08:44:11 +0100

    Added HTML Elements

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-    echo "Hier lies the klantenpagina. You can tell it's it because I put it in a seperate folder"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yo!!</title>
+</head>
+<body>
+    <?php
+        echo "Hier lies the klantenpagina. You can tell it's it because I put it in a seperate folder"
+    ?>   
+</body>
+</html>
