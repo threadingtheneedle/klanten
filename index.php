51310b6dccf60aee987f38dcf64e9f605f21a7b3
threadingtheneedle <gino.xu@edu-kw1c.nl>
2026-03-18 11:40:32 +0100

    Added CSS link

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
+<?php
+$TEXT = "Wario";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Boostraps importing-->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+     <link rel="stylesheet" href="style/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yo!!</title>
 </head>
 <body>
     <?php
-        echo "Hier lies the klantenpagina. You can tell it's it because I put it in a seperate folder"
+        echo "Lalala $TEXT with a side of mayonnaise";
     ?>   
 </body>
 </html>
