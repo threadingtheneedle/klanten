56eb36b36bfba8ca98400c1e5f4c743cbe31f81d
threadingtheneedle <gino.xu@edu-kw1c.nl>
2026-04-01 09:41:25 +0200

    attempted to add an update page

---
+++ realupdate.php
@@ -0,0 +1,19 @@
+<?php
+require "database.php";
+
+    $sql = "UPDATE INTO `eindopdrdata` (`voornaam`, `achternaam`, `email`, `geboortedatum`)
+            VALUES (:voornaam, :achternaam, :email, :geboortedatum)";
+
+    $insert = $connection->prepare($sql);
+
+    $insert->execute([
+        ":voornaam" => $_POST["voornaamInp"],
+        ":achternaam" => $_POST["achternaamInp"],
+        ":email" => $_POST["emailInp"],
+        ":geboortedatum" => $_POST["geboortedatumInp"],
+        ]);
+// auto redirect to the index
+header("Location: index.php");
+
+exit;
+?>
---
+++ update.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Boostraps importing-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserting</title>
+</head>
+<body>
+    <form method="post" action="realinsert.php">
+        <input type="text" name="voornaamInp" placeholder="Voornaam">
+        <input type="text" name="achternaamInp" placeholder="Achternaam">
+        <input type="email" name="emailInp" placeholder="Email">
+        <input type="text" name="geboortedatumInp" placeholder="Datum [YYYY-MM-DD]">
+        <button type="submit" name="insert">
+            Insert
+        </button>
+    </form>
+</body>
+</html>
